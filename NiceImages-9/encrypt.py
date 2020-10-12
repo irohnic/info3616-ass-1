acd610becd938945d06f12a837d934630d709b6f
efay8881 <efay8881@uni.sydney.edu.au>
2020-10-12 18:24:20 +1100

    Completed task 9

--- NiceImages-9/encrypt.py
+++ NiceImages-9/encrypt.py
@@ -1,31 +1,33 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-from numpy import array
 
+#This function takes data read from file, and pads it to an integer multiple of the
+#AES blocksize
+def pad_list(data):
+    requiredFill = (AES.block_size - len(data[0])) % AES.block_size
+    for _ in range(requiredFill):
+        data[0] = data[0] + b'0'
+    return(data[0])
+
+#Open, read, and close input file
 input = open("myfile.png.bin","rb")
 print("Opened input file")
 dataIn = input.readlines();
-
-maxKeyLength = int(128/8)
-key = b'INFO3616INFO3616'
-
-pad_data = lambda data: data + (AES.block_size - len(data) % AES.block_size) * b'0'
-
-cipher = AES.new(key, AES.MODE_ECB)
-print(type(dataIn))
-a = array(dataIn)
-print(a.shape)
-encryptedData = cipher.encrypt(pad_data(dataIn))
-
-print("Raw data")
-print(dataIn)
-print("Encrypted")
-print(encryptedData)
 input.close()
 print("Closed input file")
 
+#Define encryption key
+key = b'INFO3616INFO3616'
+#Generate cipher object using key
+cipher = AES.new(key, AES.MODE_ECB)
+#Pad data
+paddedData = pad_list(dataIn)
+#Encrypt data
+encryptedData = cipher.encrypt(paddedData)
+
+#Open, write, and close output file
 output = open("myfile.png.bin.enc.bin","wb")
 print("Opened output file")
-
+output.write(encryptedData)
 output.close()
 print("Closed output file")
