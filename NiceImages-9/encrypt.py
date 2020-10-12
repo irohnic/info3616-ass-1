8f9728b72810ab7a14e5e67e67a288176aea9287
efay8881 <efay8881@uni.sydney.edu.au>
2020-10-12 17:48:38 +1100

    Tried to get decryption working, still not yet functional

--- NiceImages-9/encrypt.py
+++ NiceImages-9/encrypt.py
@@ -1,5 +1,31 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from numpy import array
 
+input = open("myfile.png.bin","rb")
+print("Opened input file")
+dataIn = input.readlines();
 
-file input = open("myfile.png.bin","rb")
+maxKeyLength = int(128/8)
+key = b'INFO3616INFO3616'
 
-file output = open("myfile.png.bin.enc.bin","wb")
+pad_data = lambda data: data + (AES.block_size - len(data) % AES.block_size) * b'0'
+
+cipher = AES.new(key, AES.MODE_ECB)
+print(type(dataIn))
+a = array(dataIn)
+print(a.shape)
+encryptedData = cipher.encrypt(pad_data(dataIn))
+
+print("Raw data")
+print(dataIn)
+print("Encrypted")
+print(encryptedData)
+input.close()
+print("Closed input file")
+
+output = open("myfile.png.bin.enc.bin","wb")
+print("Opened output file")
+
+output.close()
+print("Closed output file")
--- PoorMansAES-10/main.py
+++ PoorMansAES-10/main.py
@@ -9,7 +9,7 @@ plain3 = "#    START     #".encode()
 plainX = "#     END      #".encode()
 plainStart = [plain1, plain2, plain3, plain2, plain1]
 plainEnd = [plain1, plain2, plainX, plain2, plain1]
-key = ... # I am not about to tell you!
+key = "INFO3616INFO3616" # I am not about to tell you!
 
 cipher = AES.new(key, AES.MODE_ECB)
 
@@ -18,12 +18,13 @@ def run_xor(b1, b2):
     if len(b1) != len(b2):
         print("XOR: mismatching length of byte arrays")
         os.exit(-1)
-    
+
     output = []
 
     for i in range(0, len(b1)):
         x = b1[i] ^ b2[i]
-        t = "%x" % x
+        t = "%x" % x #convert decimal to hex
+
         if len(t) == 1:
             t = "0" + t
         output.append(t)
@@ -51,9 +52,59 @@ def encrypt_input_file(filename):
 
 
 def break_input_file(filename):
-    # YOUR JOB STARTS HERE
+    emptyByte = b'\x00'
+    emptyStream = emptyByte + emptyByte + emptyByte + emptyByte + emptyByte + emptyByte + emptyByte + emptyByte + emptyByte + emptyByte + emptyByte + emptyByte + emptyByte + emptyByte + emptyByte + emptyByte
+    #print(emptyStream)
 
-    # YOUR JOB ENDS HERE
+    nonce = "0000000000000000"
+    enc_nonce = cipher.encrypt(nonce.encode())
+    print("Encrypted nonce: ",enc_nonce)
+    data = "this is a tet!@#"
+    print("Data: ",data)
+    #encoded_data = run_xor(enc_nonce,data.encode())
+    encoded_data = "4057562b7defa5579e65a89c7d75be51"
+    #encoded_data = "70e0e190d80210e37527246b67e7d488"
+    print("encoded unhexed: ", encoded_data)
+    enc_data = binascii.unhexlify(encoded_data)
+    print("Encrypted Data: ", enc_data)
+
+
+    unenc_data = binascii.unhexlify(run_xor(enc_nonce,enc_data))
+    print("Unencrypted Data: ",unenc_data)
+
+    with open(filename, "r") as infh, open("unencrypted.enc", "w") as outfh:
+        i = 0
+        for line in infh:
+            line = line.rstrip("\n")[2:]
+        #    print(line)
+            line = binascii.unhexlify(line)
+            print("-----------------")
+            print(line)
+
+            nonce = "000000000000000" + str(i)
+
+            enc_nonce =
+            #enc_nonce = cipher.encrypt(nonce.encode())
+
+            res = binascii.unhexlify(run_xor(enc_nonce,line))
+
+            print(res)
+
+            #outfh.write(str(i) + "," + res + "\n")
+            i = (i + 1) % 10
+
+
+    #print(enc_nonce)
+    # plain = "abcdefghijklmnop"
+    # print(plain)
+    # encrypted = transcrypt(nonce.encode(), plain.encode());
+    # #THIS IS WHAT WE TRY TO DECODE
+    # print(encrypted)
+    #
+    # encryptionOut = run_xor(enc_nonce,emptyStream)
+    # print(encryptionOut)
+    # print(run_xor(encrypted.encode(),encryptionOut.encode()))
+
     pass
 
 
